0d6cacc7c5d2a32fe0369d0564a762eecdd55dfa
ndsantos <n.d.santos@gmail.com>
2018-01-09 16:49:25 +0000

    hw

--- server.js
+++ server.js
@@ -57,6 +57,8 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
+    res.send('Hello World\n');
+    /*
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -75,6 +77,7 @@ app.get('/', function (req, res) {
   } else {
     res.render('index.html', { pageCountMessage : null});
   }
+  */
 });
 
 app.get('/pagecount', function (req, res) {
